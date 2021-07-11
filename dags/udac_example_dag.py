916b436d45e74e8d4e8d6b807f846dc1af455093
patkiptoo <patkiptoo@users.noreply.github.com>
2021-07-11 15:24:33 -0400

    Update udac_example_dag.py
    update schedule to top of hour

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -31,7 +31,7 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='30 * * * *',
+          schedule_interval='0 * * * *',
           catchup=False
         )
 
